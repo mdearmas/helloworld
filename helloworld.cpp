10172e03e405dfb6114d4d52acb0c25bfaf67abc
Makenzie De Armas <dearm102@mail.chapman.edu>
2019-02-06 01:43:00 +0000

    Completed the assignment.

---
+++ helloworld.cpp
@@ -0,0 +1,19 @@
+/*
+Makenzie De Armas
+ID: 2282417
+dearm102@mail.chapman.edu
+CPSC 350-01
+Assignment Number: 1 (Hello World)
+
+Purpose: Create an introductory program that prints "Hello world!" to the console.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+  cout << "Hello world!" << endl;
+  return 0;
+}
